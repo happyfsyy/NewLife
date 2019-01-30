8d957f0000eae79610d5a6e242af013cbe4fa56c
le <877105914@qq.com>
2019-01-30 12:34:41 +0800

    toolbar supplement

--- app/src/main/java/com/example/newlife/ToolbarTestAct.java
+++ app/src/main/java/com/example/newlife/ToolbarTestAct.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -18,19 +19,33 @@ public class ToolbarTestAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar_test);
         Toolbar toolbar=findViewById(R.id.toolbar_test);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
-        ActionBar actionBar=getSupportActionBar();
-        if(actionBar!=null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        }
+//        ActionBar actionBar=getSupportActionBar();
+//        actionBar.setDisplayShowTitleEnabled(false);
+
+        RadioGroup radioGroup=findViewById(R.id.toolbar_test_radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.toolbar_test_radio_message:
+                        Toast.makeText(ToolbarTestAct.this,"Message",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.toolbar_test_radio_phone:
+                        Toast.makeText(ToolbarTestAct.this,"Phone",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_test,menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.toolbar_test,menu);
+//        return true;
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
